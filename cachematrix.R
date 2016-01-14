7b26cf7b7ad6b8a14d266d174e2bc4fd9700bf8d
unknown <copyadda@gmail.com>
2016-01-14 17:34:13 +0530

    rFunction

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,31 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+##the function contains argument of x which is a matrix. 
+##It returns a list of functions
+##through this list you can set "matrix" and give Cached matrix if available
 makeCacheMatrix <- function(x = matrix()) {
-
+     m<-NULL
+     set<-function(y){
+          x<<-y
+          m<<-NULL
+     }
+     get<-function() x
+     setInverse<-function(inverse) m<<-inverse
+     getInverse<-function() m
+     list(set=set,get=get,setInverse=setInverse,getInverse=getInverse)
 }
 
 
 ## Write a short comment describing this function
-
+##this function gives the value inverse of matrix
+##if a cache value of inverse of matrix is present then it doesnt calculate the inverse of matrix
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+     ## Return a matrix that is the inverse of 'x'
+     m<-x$getInverse()
+     if(!is.null(m)) return(m)
+     mat<-x$get()
+     m<-solve(mat)
+     x$setInverse(m)
+     m
 }
